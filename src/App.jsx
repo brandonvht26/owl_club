342a9ac6f9ac4db7dcd136a889bfbed12327995c
brandonvht26 <brandon.v.huera.t@gmail.com>
2025-06-26 23:54:19 -0500

    Header, cambios en header.css e index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // src/App.js (Ejemplo)
 import React from 'react';
-import Galeria from './components/Galeria/Galeria'; // Importa el componente Galeria
+import Galeria from './components/Galeria/Galeria'; 
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services'
-import Download from './components/Download/Download'; // Importa el componente Download
+import Download from './components/Download/Download';
+import Header from './components/Header/Header';
 // Importa otros componentes si los tienes, como HeroSection, MemberCard, ContactForm, etc.
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Hero />
       <Services/>
       <Galeria />
