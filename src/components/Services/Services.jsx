0e714e0db7e15127994e6f3de8eaa9ad5b0557f9
mtemox <materchico14@gmail.com>
2025-07-11 00:14:10 -0500

    swiper

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
-import './Services.css'
+import './Services.css';
+
+// Swiper imports
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const temas = ["Matemática", "Física", "Biología", "Artes", "Inglés", "ECA"];
 
 const Services = () => {
     useEffect(() => {
-        AOS.init({ duration: 1000, once: true }); 
+        AOS.init({ duration: 1000, once: true });
     }, []);
 
     return (
@@ -14,43 +23,62 @@ const Services = () => {
             </div>
             <div className="search">
                 <h2 className="search_box">
-                    <i className= "fas fa-search"></i> Escribe tu pregunta aquí...
+                    <i className="fas fa-search"></i> Escribe tu pregunta aquí...
                 </h2>
             </div>
             <div className="topics_container">
                 <h3 className="container_title">Temas recientes</h3>
             </div>
-            <div className="topics_grid">{["Matemática", "Física", "Biología", "Artes", "Inglés", "ECA"].map((tema) => (
-                <div className="topic_item" key={tema} data-aos="zoom-in">
-                    <div className="topic_box">
-                    <div className="header">
-                        <h4 className="title_box">
-                        <i className="fas fa-circle"></i>
-                        <div className="header_text">
-                            <span className="header_title">Header</span>
-                            <span className="header_subtitle">Subhead</span>
+            {/* Swiper Carrusel */}
+            <Swiper
+                modules={[Navigation, Pagination]}
+                spaceBetween={24}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                breakpoints={{
+                    640: { slidesPerView: 2 },
+                    1024: { slidesPerView: 3 },
+                }}
+                className="topics_swiper"
+            >
+                {temas.map((tema, idx) => (
+                    <SwiperSlide key={tema}>
+                        <div
+                            className="topic_item"
+                            data-aos="zoom-in"
+                            data-aos-delay={100 * idx}
+                        >
+                            <div className="topic_box">
+                                <div className="header">
+                                    <h4 className="title_box">
+                                        <i className="fas fa-circle"></i>
+                                        <div className="header_text">
+                                            <span className="header_title">Header</span>
+                                            <span className="header_subtitle">Subhead</span>
+                                        </div>
+                                    </h4>
+                                    <div className="label_menu_box">
+                                        <div className="box_label">
+                                            <i className="far fa-hand-pointer icon-hand"></i> {tema}
+                                        </div>
+                                        <i className="fas fa-ellipsis-v options_icon"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="content_box">
+                                <div className="topic_content">
+                                    <h5 className="content_title">TEMA</h5>
+                                    <h6 className="content_subtitle">Subtema</h6>
+                                    <p className="content">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                        </h4>
-                        <div className="label_menu_box">
-                        <div className="box_label">
-                            <i className="far fa-hand-pointer icon-hand"></i> {tema}
-                        </div>
-                        <i className="fas fa-ellipsis-v options_icon"></i>
-                        </div>
-                    </div>
-                    </div>
-                    <div className="content_box">
-                    <div className="topic_content">
-                        <h5 className="content_title">TEMA</h5>
-                        <h6 className="content_subtitle">Subtema</h6>
-                        <p className="content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                        </p>
-                    </div>
-                    </div>
-                </div>
+                    </SwiperSlide>
                 ))}
-        </div>
+            </Swiper>
         </section>
     )
 }
